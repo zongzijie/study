979d8f09ad37490c075bef734627c20d97df03a5
wojiejie <328308304@qq.com>
2019-10-06 21:45:28 +0800

    clear

--- webpack-sample-project/app/main.js
+++
@@ -1,8 +0,0 @@
-// const greeter = require('./Greeter.js');
-// document.querySelector("#root").appendChild(greeter());
-import React from 'react'
-import {render} from 'react-dom'
-import Greeter from './Greeter'
-import './main.css';
-
-render(<Greeter />,document.getElementById('root'));
