965574fc3c1c443331d5fb686043d07a62378fca
wojiejie <328308304@qq.com>
2019-10-17 16:25:50 +0800

    fix

---
+++ CSharp/WebApplication3/WebApplication3/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication3
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            //
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "Default", 
+                routeTemplate:"api/{controller}/{id}",
+                defaults: new {id = RouteParameter.Optional}
+            );
+        }
+    }
+}
---
+++ CSharp/WebApplication3/WebApplication3/Controllers/OrderController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Http;
+
+namespace WebApplication3.Controllers
+{
+    public class OrderController : ApiController
+    {
+        [HttpGet]
+        public string GetOrder()
+        {
+            return "Success";
+        }
+    }
+}
